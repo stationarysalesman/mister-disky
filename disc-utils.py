8d760c10c2836a641c682b0854434b4321f765ad
Tyler Camp <tylerc5@illinois.edu>
2017-10-07 22:18:22 -0500

    rotational correlation time

--- disc-utils.py
+++ disc-utils.py
@@ -18,7 +18,6 @@ def calcNormal(points):
 def calcAxes(points):
     """Calculate a local coordinate frame for an object given points in a plane         of that object."""
     y = calcNormal(points)
-    print "y is: " + str(y)
     theta = np.pi / 4
     # Rotation matrix for negative z-axis
     R = np.array((
@@ -26,11 +25,22 @@ def calcAxes(points):
         (0,-np.cos(theta),np.sin(theta)),
         (0,-np.sin(theta),-np.cos(theta)))) 
     v = np.matmul(R, y)
-    print v
     cross = np.cross(v,y)
     x = np.divide(cross, np.linalg.norm(cross))
     z = np.cross(x,y)
     return (x, y, z)
+
+xGlobal = np.array((1,0,0))
+yGlobal = np.array((0,1,0))
+zGlobal = np.array((0,0,1))
+
+def calcRotation(x,y,z):
+    """Calculate the angle between a local coordinate frame, defined by x,y,z, 
+        and a global coordinate frame defined by x=(1,0,0), y=(0,1,0), and 
+        z=(0,0,1)."""
+    thetaX = np.arccos(np.vdot(x, xGlobal))
+    thetaY = np.arccos(np.vdot(y, yGlobal))
+    thetaZ = np.arccos(np.vdot(z, zGlobal))
 
 # test
 """
@@ -45,3 +55,38 @@ ax.plot([0,y[0]],[0,y[1]],[0,y[2]], color='red')
 ax.plot([0,z[0]],[0,z[1]],[0,z[2]], color='green')
 plt.show()
 """
+
+# test reading
+u = mda.Universe('ND.psf','ND.dcd')
+selection_str = 'resname DLPE DMPC DPPC GPG LPPC PALM PC PGCL POPC POPE'
+lipids = u.select_atoms(selection_str, updating=True)
+dt = 10e-12
+xs = np.zeros(len(u.trajectory))
+xangles = np.zeros(len(u.trajectory))
+yangles = np.zeros(len(u.trajectory))
+zangles = np.zeros(len(u.trajectory))
+for i,ts in enumerate(u.trajectory):
+    x,y,z = calcAxes(lipids.positions)
+    xangles[i] = np.arccos(np.vdot(x, xGlobal))
+    yangles[i] = np.arccos(np.vdot(y, yGlobal))
+    zangles[i] = np.arccos(np.vdot(z, zGlobal))
+    xs[i] = dt * i 
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(xs, xangles, color='red')
+ax.plot(xs, yangles, color='green')
+ax.plot(xs, zangles, color='blue')
+plt.show()   
+
+xRotDisp = np.sum(np.absolute(np.diff(xangles)))
+yRotDisp = np.sum(np.absolute(np.diff(yangles)))
+zRotDisp = np.sum(np.absolute(np.diff(zangles)))
+delT = dt * len(u.trajectory)
+delTdelX = np.divide(delT, xRotDisp)
+delTdelY = np.divide(delT, yRotDisp)
+delTdelZ = np.divide(delT, zRotDisp)
+print('Rotational correlation times (x,y,z): {} {} {}'.format(delTdelX,
+                                                              delTdelY,
+                                                              delTdelZ))
+
