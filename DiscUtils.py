46c02e083cb2e36132b281d440da7aeb8a646d99
Tyler Camp <tyler@cjbaker3-mac.life.uiuc.edu>
2017-10-13 16:34:56 -0500

    new vector generators

---
+++ DiscUtils.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import MDAnalysis as mda
+
+def calcNormal(points):
+    """Use least-squares regression to get a plane from a set of points. 
+        Return the normal of that plane."""
+    A,z = np.hsplit(points, [2])
+    l = len(points)
+    ones = np.ones(l).reshape(l, 1)
+    A = np.hstack((A,ones))
+    x,residuals,rank,s = np.linalg.lstsq(A, z)
+    x = np.append(x[:2], 1)
+    n = np.divide(x, np.linalg.norm(x))
+    return n
+
+
+def calcAxes(points):
+    """Calculate a local coordinate frame for an object given points in a plane         of that object."""
+    y = calcNormal(points)
+    theta = np.pi / 4
+    # Rotation matrix for negative z-axis
+    R = np.array((
+        (1,0,0),
+        (0,-np.cos(theta),np.sin(theta)),
+        (0,-np.sin(theta),-np.cos(theta)))) 
+    v = np.matmul(R, y)
+    cross = np.cross(v,y)
+    x = np.divide(cross, np.linalg.norm(cross))
+    z = np.cross(x,y)
+    return (x, y, z)
+
+
+def calcXAxis(lipids, helixResidues):
+    """Calculate a faux-X-axis; that is, a vector pointing from the 
+    center of mass of the lipids to the center of mass of a single 
+    helical turn in the MSP."""
+    lipidR = calcCenterOfMass(lipids)
+    helixR = calcCenterOfMass(helixResidues)
+    x = (helixR - lipidR) 
+    return x / np.linalg.norm(x)
+    
+def calcCenterOfMass(atoms):
+    """Calculate the center of mass of a given set of atoms."""
+    M = 0.0
+    s = np.zeros((3)) 
+    for a in atoms:
+        s += a.mass * a.position 
+        M += a.mass
+    s /= M
+    return s
+ 
+xGlobal = np.array((1,0,0))
+yGlobal = np.array((0,1,0))
+zGlobal = np.array((0,0,1))
+
+def calcRotation(x,y,z):
+    """Calculate the angle between a local coordinate frame, defined by x,y,z, 
+        and a global coordinate frame defined by x=(1,0,0), y=(0,1,0), and 
+        z=(0,0,1)."""
+    thetaX = np.arccos(np.vdot(x, xGlobal))
+    thetaY = np.arccos(np.vdot(y, yGlobal))
+    thetaZ = np.arccos(np.vdot(z, zGlobal))
+
+# test
+"""
+points = np.array(((1,0,0),(0,1,0),(0,0,0)))
+x,y,z = calcAxes(points)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+xs,ys,zs = np.hsplit(points,[1,2])
+ax.scatter(xs,ys,zs)
+ax.plot([0,x[0]],[0,x[1]],[0,x[2]])
+ax.plot([0,y[0]],[0,y[1]],[0,y[2]], color='red')
+ax.plot([0,z[0]],[0,z[1]],[0,z[2]], color='green')
+plt.show()
+"""
+
---
+++ MisterDisky.py
@@ -0,0 +1,73 @@
+from DiscUtils import *
+
+def main():
+    u = mda.Universe('ND.psf','ND.dcd')
+    lipid_str = 'resname DLPE DMPC DPPC GPG LPPC PALM PC PGCL POPC POPE'
+    helix_str = 'bynum 1352:1389' # a single turn of MSP1, near x-axis at start
+    #lipids = u.select_atoms(lipid_str, updating=True)
+    #helixResidues = u.select_atoms(helix_str, updating=True)
+    x1 = u.select_atoms('bynum 1771', updating=True)
+    x2 = u.select_atoms('bynum 2791', updating=True)
+    y1 = u.select_atoms('bynum 3211', updating=True)
+    y2 = u.select_atoms('bynum 2247', updating=True)
+    dt = 1e-9
+    xs = np.zeros(len(u.trajectory))
+    pitch = np.zeros(len(u.trajectory))
+    yaw = np.zeros(len(u.trajectory))
+    roll = np.zeros(len(u.trajectory))
+    for i,ts in enumerate(u.trajectory):
+        # Get local coordinate axes 
+        x1_pos = x1[0].position
+        x2_pos = x2[0].position
+        x = (x1_pos - x2_pos) / (np.linalg.norm(x1_pos - x2_pos))
+        y1_pos = y1[0].position
+        y2_pos = y2[0].position
+        y = (y1_pos - y2_pos) / (np.linalg.norm(y1_pos - y2_pos))
+        z = np.cross(x, y)
+        print('x: {}'.format(x)) 
+        print('y: {}'.format(y)) 
+        print('z: {}'.format(z)) 
+        # Compute the yaw, i.e. rotation of x-axis about the global y-axis
+        xhat = np.copy(x)
+        xhat[1] = 0.0 # project onto xz-plane
+        xhat /= np.linalg.norm(xhat)
+        yaw[i] = np.arccos(np.vdot(xhat, xGlobal)) # a dot b = |a||b|cos(theta)
+       
+        # Compute the pitch, i.e. rotation of z-axis about the global x-axis 
+        zhat = np.copy(z)
+        zhat[0] = 0.0 # project onto zy-plane
+        zhat /= np.linalg.norm(zhat)
+        pitch[i] = np.arccos(np.vdot(zhat, zGlobal))
+        
+        # Compute the roll, i.e. rotation of y-axis about the global z-axis
+        yhat = np.copy(y)
+        yhat[2] = 0.0 # project onto xy-plane
+        yhat /= np.linalg.norm(yhat)
+        roll[i] = np.arccos(np.vdot(yhat, yGlobal))
+       
+        # X points for graphing 
+        xs[i] = dt * i 
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(xs, roll, color='red', label='Roll Angle')
+    ax.plot(xs, pitch, color='green', label='Pitch Angle')
+    ax.plot(xs, yaw, color='blue', label='Yaw Angle')
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    plt.show()   
+
+    rollRotDisp = np.sum(np.absolute(np.diff(roll)))
+    pitchRotDisp = np.sum(np.absolute(np.diff(pitch)))
+    yawRotDisp = np.sum(np.absolute(np.diff(yaw)))
+    delT = dt * len(u.trajectory)
+    delTdelRoll = np.divide(delT, rollRotDisp)
+    delTdelPitch = np.divide(delT, pitchRotDisp)
+    delTdelYaw = np.divide(delT, yawRotDisp)
+    print('Roll rotational correlation time: {}'.format(delTdelRoll))
+    print('Pitch rotational correlation time: {}'.format(delTdelPitch))
+    print('Yaw rotational correlation time: {}'.format(delTdelYaw))
+
+if __name__ == '__main__':
+    main()
+
